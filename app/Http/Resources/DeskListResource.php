0bef211c722e325930beb0e9e178b0691a414b57
Anastasya <aamokeeva@yandex.ru>
2024-01-16 19:56:47 +0300

    Создает Json Resources для списка записей и единичной записи

---
+++ app/Http/Resources/DeskListResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\CarbonImmutable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DeskListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'created_at' => CarbonImmutable::parse($this->created_at)->toDateTimeString(),
+        ];
+    }
+}
