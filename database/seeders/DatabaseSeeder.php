18d2f2765b581f8568c8b4f8361bcbf2b0bed453
Anastasya <aamokeeva@yandex.ru>
2024-01-16 18:17:15 +0300

    Создает модели, миграции и фабрики для моделей

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Card;
+use App\Models\Desk;
+use App\Models\DeskList;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+         Desk::factory(10)->create();
+         DeskList::factory(50)->create();
+         Card::factory(50)->create();
+         Task::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
